2e088ea541e0381f984b9cb61e0c0adc75ca5179
Michaël Zasso <targos@protonmail.com>
2020-10-20 15:40:04 +0200

    exercises 1-4

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -5,25 +5,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
-  //
+  const div = document.createElement('div')
+
   // 🐨 append the div to document.body (💰 document.body.append)
-  //
+  document.body.append(div)
+
   // 🐨 use ReactDOM.render to render the <Counter /> to the div
+  ReactDOM.render(<Counter />, div)
+
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+  const [decrement, increment] = div.querySelectorAll('button')
+
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
-  //
+  const message = div.firstChild.querySelector('div')
+
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
+
+  const clickEvent = new MouseEvent('click', {bubbles: true, button: 0})
+
   // 🐨 click the increment button (💰 increment.click())
+  increment.dispatchEvent(clickEvent)
   // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 1')
   // 🐨 click the decrement button (💰 decrement.click())
+  decrement.dispatchEvent(clickEvent)
   // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+  expect(message.textContent).toBe('Current count: 0')
 })
 
 /* eslint no-unused-vars:0 */
--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,24 +4,36 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
+const buildLoginForm = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
+  },
+})
+
 test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+
+  const {username, password} = buildLoginForm()
+
+  const usernameField = screen.getByLabelText(/username/i)
+  userEvent.type(usernameField, username)
+
+  const passwordField = screen.getByLabelText(/password/i)
+  userEvent.type(passwordField, password)
+
+  const submitButton = screen.getByRole('button', {name: /submit/i})
+  userEvent.click(submitButton)
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
 /*
